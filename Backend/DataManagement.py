3c99ff31375253f76f8d606b3b0b83858cb647ec
Hasina <randriatsarafar@gmail.com>
2023-06-14 15:08:48 +0300

    Final code

--- Backend/DataManagement.py
+++ Backend/DataManagement.py
@@ -3,33 +3,13 @@ from Backend.utils import getData, percentofAfricanAmericanStudents, sort_statis
 
 def main():
 
-    # directory = 'data/'
-    # for filename in os.listdir(directory):
-    #     file_path = os.path.join(directory, filename)
-    #     if os.path.isfile(file_path):
-    #         with open(file_path, 'r') as file:
-    #             content = json.load(file)
-    #         statistics_.append(list(percentofAfricanAmericanStudents(filename[:-5], content, [2023, 2022, 2021, 2020]).items()))
-    #
-    #
-    # sorted = sort_statistics(statistics_)
-    #
-    # for row in sorted:
-    #     print(row)
-
-
     state_abr_list = \
         ['AL', 'AK', 'AZ', 'AR', 'CA', 'CO', 'CT', 'DE', 'FL', 'GA', 'HI', 'ID', 'IL', 'IN', 'IA', 'KS', 'KY'
             , 'LA', 'ME', 'MD', 'MA', 'MI', 'MN', 'MS', 'MO', 'MT', 'NE', 'NV', 'NH', 'NJ', 'NM', 'NY', 'NC', 'ND'
             , 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX', 'UT', 'VT', 'VA', 'WA', 'WV', 'WI', 'WY', 'DC']
 
-
-    import time
-
     content = []
     statistics_ = []
-
-    start = time.time()
 
     for state in state_abr_list:
         content.append([state, getData(state)])
@@ -44,22 +24,6 @@ def main():
 
     for row in sorted_list:
         print(row)
-
-    end = time.time()
-    print(end - start)
-
-    # import json
-    #
-    # with open('data.json', 'w') as f:
-    #     json.dump(data, f)
-    #
-    # sorted_list = sort_statistics(statistics_)
-    #
-    # print(type(sorted_list))
-    # print(sorted_list)
-    #
-    # for row in sorted_list:
-    #     print(row)
 
     return
 
