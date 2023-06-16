b6b67a1eb2b5259cad3323f34f21d8fd2d1003fd
Hasina <randriatsarafar@gmail.com>
2023-06-16 22:24:40 +0300

    Upgrading speed performance

--- Backend/utils.py
+++ Backend/utils.py
@@ -1,3 +1,6 @@
+import asyncio
+
+import aiohttp
 import requests
 import datetime
 
@@ -14,22 +17,36 @@ def computePercentageFromNumber(totalNumber, number):
     except:
         return 0
 
+
 # Retrieve Data from API url
 # Params (STR)Abreviation of State, (STR)AppID, (STR)appKey, (INT)number of object perPage
-def getData(stateAbr, appID='052d9b7b', appKey='2bb041493517101dba0e91f04aee75b0', perPage=20):
-    url = 'https://api.schooldigger.com/v2.0/schools?st=' + stateAbr + '&perPage=' + str(
-        perPage) + '&appID=' + appID + '&appKey=' + appKey
-    response = requests.get(url)
-    if response.status_code == 200:
-        return response.json()
-    else:
-        return False
+# def getData(stateAbr, appID='052d9b7b', appKey='2bb041493517101dba0e91f04aee75b0', perPage=20):
+#     url = 'https://api.schooldigger.com/v2.0/schools?st=' + stateAbr + '&perPage=' + str(
+#         perPage) + '&appID=' + appID + '&appKey=' + appKey
+#     response = requests.get(url)
+#     if response.status_code == 200:
+#         return response.json()
+#     else:
+#         return False
 
+async def getData(stateAbrs, appID='052d9b7b', appKey='2bb041493517101dba0e91f04aee75b0', perPage=20):
+    urls = [[stateAbr, 'https://api.schooldigger.com/v2.0/schools?st=' + stateAbr + '&perPage=' + str(
+        perPage) + '&appID=' + appID + '&appKey=' + appKey] for stateAbr in stateAbrs]
+
+    async with aiohttp.ClientSession() as session:
+        tasks = [asyncFetch(session, url[1], url[0]) for url in urls]
+        responses = await asyncio.gather(*tasks)
+    return responses
+
+
+async def asyncFetch(session, url,stateAbr):
+    async with session.get(url) as response:
+        var = await response.json()
+        return [stateAbr, var]
 
 # Compute the Percentage
 # Params (STR) Abreviation of State, (Json)data associated with the state, (INT, [INT])year can be list of int
 def percentofAfricanAmericanStudents(stateAbr, data, year=datetime.date.today().year, is_list=False):
-
     if type(year) in (list, tuple, dict): is_list = True
 
     if data:
@@ -44,7 +61,7 @@ def percentofAfricanAmericanStudents(stateAbr, data, year=datetime.date.today().
                 for schoolYearlyDetail in schoolYearlyDetails:
 
                     if is_list:
-                        if schoolYearlyDetail['year'] in year :
+                        if schoolYearlyDetail['year'] in year:
                             numberOfStudents = schoolYearlyDetail['numberOfStudents']
                             totalStudent += numberOfStudents
                             numberOfAfricanAmericanStudents = computeNumberFromPercentage(numberOfStudents,
@@ -76,7 +93,7 @@ def percentofAfricanAmericanStudents(stateAbr, data, year=datetime.date.today().
 
 
 # Sort the Array of the statistics of all school
-def sort_statistics(table, params={}, top=5,column_name='percentofAfricanAmericanStudents'):
+def sort_statistics(table, params={}, top=5, column_name='percentofAfricanAmericanStudents'):
     if 'top' in params.keys():
         top = int(params['top'])
     if 'column_name' in params.keys():
@@ -89,4 +106,3 @@ def sort_statistics(table, params={}, top=5,column_name='percentofAfricanAmerica
                 table[j], table[j + 1] = table[j + 1], table[j]
     table = table[::-1]
     return table[:top]
-
